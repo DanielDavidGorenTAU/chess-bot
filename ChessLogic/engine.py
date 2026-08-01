366ede0627a6c7f2e091eeb9eacf1867c9057e8e
michaelkiner <kiner@mail.tau.ac.il>
2026-08-01 14:19:16 +0300

    chess-engine stub class

---
+++ ChessLogic/engine.py
@@ -0,0 +1,7 @@
+class ChessEngine():
+    def choose_move(fen: str) -> str:
+        """
+        Uses real chess engine to decide on move based on current board FEN string
+        Returns move in UCI format e.g "e2e4", "e7e8q"
+        """
+        return ""
