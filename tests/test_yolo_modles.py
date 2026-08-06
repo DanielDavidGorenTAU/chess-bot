26595093f506a43a9a1440e4abfb45e35ea3885d
Michael Kiner <kiner@mail.tau.ac.il>
2026-08-06 15:03:45 +0300

    test

---
+++ tests/test_yolo_modles.py
@@ -0,0 +1,21 @@
+
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+
+from yolo.yolo_model import YoloModel
+from ZED.cameralib import Camera
+
+if __name__ == "__main__":
+    image_path = "/home/checkmate/Documents/chess-bot/yolo/game_photos/00_20260806_125747_340.png"
+    with Camera() as camera:
+        yolo_model = YoloModel(model_name="advanced", camera=camera, save_regularly=False)
+        yolo_model.predict(image_path)
+        
+
+    
+    
