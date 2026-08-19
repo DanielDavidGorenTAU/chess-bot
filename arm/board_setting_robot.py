630e8988d9b677a7418b356b5dd29a81f71b5b19
Michael Kiner <kiner@mail.tau.ac.il>
2026-08-19 10:32:03 +0300

    .

--- arm/board_setting_robot.py
+++ arm/board_setting_robot.py
@@ -37,21 +37,21 @@ class BoardSettingArm(BoardSettingRobot):
         # translate to robot coordinates
         head_pos = self.robot_hardware.normalize_pos(head_pos)
         base_pos = self.robot_hardware.normalize_pos(base_pos)
+
         print(f"orientation: {orientation}, head: {head_pos}, base: {base_pos}")
         dx, dy, dz = [head_pos[i] - base_pos[i] for i in range(3)]
         drz = math.degrees(math.atan2(dx, dy))
-        #elevation = math.degrees(math.atan2(dz, math.hypot(dx, dy)))
-        #is_standing = (45.0 < elevation < 135.0)
         is_standing = (orientation == Orientation.STANDING)
         HEAD_BIAS = 0.6
+        
         pickup_pos = weighted_avg(head_pos[:3], base_pos[:3], HEAD_BIAS)
         if is_standing:
             pickup_pos = [*pickup_pos[:2], self.robot_hardware.grip_height[PieceType.PAWN] + GRIP_RELEASE_HEIGHT, *self.robot_hardware.down_orientation] # always the height of the shortest piece
             if abs(head_pos[Y] - base_pos[Y]) > 0.015: # too far
                 pickup_pos[Y] = base_pos[Y] - 0.010
-                print("too far!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!11")
+                print("points are too far")
             if abs(head_pos[Y] - base_pos[Y]) < 0.01: # too close
-                print("too close!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                print("points are too close")
                 pickup_pos[Y] = head_pos[Y]
         else:
             pickup_pos = [pickup_pos[X], pickup_pos[Y],self.robot_hardware.table_height+0.005, *self.robot_hardware.get_rotated_tcp_orientation(Rz=drz+180)]
